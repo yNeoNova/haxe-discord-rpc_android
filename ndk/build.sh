754763d5f3eecd36c79ce2fe51b07a45471e646a
Leo <sjsj79495@gmail.com>
2025-07-08 09:41:44 -0300

    Update build.sh

--- ndk/build.sh
+++ ndk/build.sh
@@ -3,10 +3,10 @@
 echo "[NDK-BUILD] Building .so for armeabi-v7a and arm64-v8a..."
 
 # Build armeabi-v7a
-APP_ABI=armeabi-v7a ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk APP_PLATFORM=android-21 || exit 1
+APP_ABI=armeabi-v7a ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=ndk/Android.mk APP_PLATFORM=android-21 || exit 1
 
 # Build arm64-v8a
-APP_ABI=arm64-v8a ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk APP_PLATFORM=android-21 || exit 1
+APP_ABI=arm64-v8a ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=ndk/Android.mk APP_PLATFORM=android-21 || exit 1
 
 # Create output folders if missing
 mkdir -p lib/android/armeabi-v7a
